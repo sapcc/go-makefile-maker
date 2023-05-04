54a2e8451b7378a688730144c66277d5c2d52d38
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-05-04 15:44:10 +0200

    docker: update base image to fix potential CVEs

--- internal/dockerfile/docker.go
+++ internal/dockerfile/docker.go
@@ -98,7 +98,9 @@ FROM alpine:%[2]s
 
 RUN addgroup -g 4200 appgroup
 RUN adduser -h /home/appuser -s /sbin/nologin -G appgroup -D -u 4200 appuser
-RUN apk add --no-cache --no-progress%[5]s
+# upgrade all installed packages to fix potential CVEs in advance
+RUN apk upgrade --no-cache --no-progress \
+  && apk add --no-cache --no-progress%[5]s
 COPY --from=builder /pkg/ /usr/
 
 ARG BININFO_BUILD_DATE BININFO_COMMIT_HASH BININFO_VERSION
