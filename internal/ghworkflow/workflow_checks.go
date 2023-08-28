46b9cea0e89a6968603c488dc4fed634cef5fb15
Sandro Jäckel <sandro.jaeckel@sap.com>
2023-08-28 14:15:06 +0200

    Deny busl license

--- internal/ghworkflow/workflow_checks.go
+++ internal/ghworkflow/workflow_checks.go
@@ -33,7 +33,7 @@ func checksWorkflow(cfg *core.GithubWorkflowConfiguration, ignoreWords []string)
 				"base-ref":         fmt.Sprintf("${{ github.event.pull_request.base.sha || '%s' }}", cfg.Global.DefaultBranch),
 				"head-ref":         "${{ github.event.pull_request.head.sha || github.ref }}",
 				"fail-on-severity": "moderate",
-				"deny-licenses":    "AGPL-1.0, AGPL-3.0, GPL-1.0, GPL-2.0, GPL-3.0, LGPL-2.0, LGPL-2.1, LGPL-3.0",
+				"deny-licenses":    "AGPL-1.0, AGPL-3.0, GPL-1.0, GPL-2.0, GPL-3.0, LGPL-2.0, LGPL-2.1, LGPL-3.0, BUSL-1.1",
 			},
 		})
 
