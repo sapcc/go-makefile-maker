37efa102fde5699a20cdd51166b54628aad40bec
Stefan Majewsky <stefan.majewsky@sap.com>
2023-09-08 09:53:27 +0200

    bump Go to 1.21.1

--- internal/core/constants.go
+++ internal/core/constants.go
@@ -16,7 +16,7 @@ package core
 
 const (
 	DefaultAlpineImage       = "3.18"
-	DefaultGolangImagePrefix = "1.21.0-alpine"
+	DefaultGolangImagePrefix = "1.21.1-alpine"
 
 	DefaultGoVersion           = "1.21"
 	DefaultPostgresVersion     = "12"
