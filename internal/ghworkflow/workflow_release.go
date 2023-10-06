b9427f95847bbdd544e29dd1ec2bbd844f2d3479
Muhammad Talal Anwar <muhammad.talal.anwar@sap.com>
2023-10-06 22:18:13 +0200

    Revert "GoReleaser: use `write` scope for contents"
    This reverts commit d9374992c0a5aaf4ba668b733a30325a2b30e4a4.

--- internal/ghworkflow/workflow_release.go
+++ internal/ghworkflow/workflow_release.go
@@ -23,7 +23,7 @@ func releaseWorkflow(cfg *core.GithubWorkflowConfiguration) {
 		return
 	}
 
-	w.Permissions.Contents = tokenScopeWrite
+	w.Permissions.Contents = tokenScopeRead
 	w.Permissions.Packages = tokenScopeWrite
 
 	w.On.Push.Branches = nil
