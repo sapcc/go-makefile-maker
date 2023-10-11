808b496003965371daebb82119233b14088860ef
Stefan Majewsky <stefan.majewsky@sap.com>
2023-10-11 10:03:44 +0200

    bump Go to 1.21.3 to address CVE-2023-44487

--- internal/core/constants.go
+++ internal/core/constants.go
@@ -16,7 +16,7 @@ package core
 
 const (
 	DefaultAlpineImage       = "3.18"
-	DefaultGolangImagePrefix = "1.21.2-alpine"
+	DefaultGolangImagePrefix = "1.21.3-alpine"
 
 	DefaultGoVersion           = "1.21"
 	DefaultPostgresVersion     = "12"
