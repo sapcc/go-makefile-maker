55fec52535fa4ae02b47b6458249463bcda31cfb
Sandro Jäckel <sandro.jaeckel@sap.com>
2024-02-07 11:55:43 +0100

    dockerfile: check if binary can be executed in final container

--- internal/dockerfile/docker.go
+++ internal/dockerfile/docker.go
@@ -111,6 +111,8 @@ RUN %[5]s
 COPY --from=builder /etc/ssl/certs/ /etc/ssl/certs/
 COPY --from=builder /etc/ssl/cert.pem /etc/ssl/cert.pem
 COPY --from=builder /pkg/ /usr/
+# make sure the binary can be executed
+RUN %[10]s --help 2>/dev/null
 
 ARG BININFO_BUILD_DATE BININFO_COMMIT_HASH BININFO_VERSION
 LABEL source_repository="%[6]s" \
