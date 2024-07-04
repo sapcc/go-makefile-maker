8ae357e269f844d0a66fe0fc51ec23a9efac3566
Sandro Jäckel <sandro.jaeckel@sap.com>
2024-07-04 13:36:59 +0200

    Bump golang to 1.22.5

--- internal/core/constants.go
+++ internal/core/constants.go
@@ -16,7 +16,7 @@ package core
 
 const (
 	DefaultAlpineImage         = "3.20"
-	DefaultGoVersion           = "1.22.4"
+	DefaultGoVersion           = "1.22.5"
 	DefaultPostgresVersion     = "16"
 	DefaultLinkerdAwaitVersion = "0.2.7"
 	DefaultGitHubComRunnerType = "ubuntu-latest"
