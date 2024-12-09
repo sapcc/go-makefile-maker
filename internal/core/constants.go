3542f2dc685506aa8ef2846bdb7f6af5e578d601
Sandro Jäckel <sandro.jaeckel@sap.com>
2024-12-09 16:55:06 +0100

    Update to alpine 3.21

--- internal/core/constants.go
+++ internal/core/constants.go
@@ -4,7 +4,7 @@
 package core
 
 const (
-	DefaultAlpineImage         = "3.20"
+	DefaultAlpineImage         = "3.21"
 	DefaultGoVersion           = "1.23.4"
 	DefaultPostgresVersion     = "17"
 	DefaultLinkerdAwaitVersion = "0.2.7"
