c51e3caebec5fb351d2ea65fd1aa502149db3965
Stefan Majewsky <stefan.majewsky@sap.com>
2025-02-05 10:11:55 +0100

    bump Go to 1.23.6

--- internal/core/constants.go
+++ internal/core/constants.go
@@ -5,7 +5,7 @@ package core
 
 const (
 	DefaultAlpineImage         = "3.21"
-	DefaultGoVersion           = "1.23.5"
+	DefaultGoVersion           = "1.23.6"
 	DefaultPostgresVersion     = "17"
 	DefaultLinkerdAwaitVersion = "0.2.7"
 	DefaultGitHubComRunsOn     = "ubuntu-latest"
